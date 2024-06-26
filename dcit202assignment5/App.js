45752b72e0fa9e8b7268e006ba80a7158491cbcb
JD-2104 <jdanso019@st.ug.edu.gh>
2024-06-26 18:09:04 +0000

    add

--- dcit202assignment5/App.js
+++ dcit202assignment5/App.js
@@ -4,18 +4,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SettingsScreen from './Screens/SettingsScreen'
 import HomeScreen from './Screens/HomeScreen'
+import { ThemeProvider, ThemeContext } from './ThemeContext';
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <>
-    <NavigationContainer>
-      <Stack.Navigator>        
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator>        
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
     </>
     
   );
