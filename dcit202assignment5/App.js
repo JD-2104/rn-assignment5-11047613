bccfcba8f7f47a5bb89044c2f0b9aea04d1c803e
JD-2104 <jdanso019@st.ug.edu.gh>
2024-06-26 17:27:36 +0000

    changes

--- dcit202assignment5/App.js
+++ dcit202assignment5/App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homepage from './Homepage';
-import Settings from './Settings';
+import { createStackNavigator } from '@react-navigation/stack';
+import SettingsScreen from './Screens/SettingsScreen'
+import HomeScreen from './Screens/HomeScreen'
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator()
 
-function App() {
+export default function App() {
   return (
+    <>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Homepage} />
-        <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Navigator>        
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </>
+    
   );
 }
 
-export default App;
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
